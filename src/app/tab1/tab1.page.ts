19c00f90689b3aa1030cdb4748a8141be46d2d10
Paulo Belfi <paulobelfi@hotmail.com>
2019-10-09 18:53:23 -0300

    Refactor index

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
+import { TimelineLite, Back, Power1, SlowMo, TimelineMax } from 'gsap'
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  menu = new TimelineMax({paused:false, reversed:true});
+  formRating = 3;
+  ngOnInit(){
+    this.createMenuAnim()
+  }
+  onCreate(){
 
-  constructor() {}
+  }
 
+  createMenuAnim(){
+    this.menu.to(".full-card", .25, {ease:"Bounce.easeOut", y:-30},0)
+  }
 }
